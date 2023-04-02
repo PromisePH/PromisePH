373c89ca20ffd9c633ea6bcd3dd606f74d7cfe10
Arnel Jan Sarmiento <rneljan@gmail.com>
2023-04-02 19:55:01 +0800

    Logout Integration

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { signOut } from 'firebase/auth';
+import { useNavigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+import { auth } from '../../firebase/firebase';
 
 function Home() {
-    return ( 
+    const [user] = useAuthState(auth);
+
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (!user) {
+            navigate('/login');
+        }
+    }, [user]);
+
+    const handleLogout = async () => {
+        try {
+            await signOut(auth);
+        }
+        catch (error) {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage);
+        }
+    };
+
+    return (
         <main>
             <h1>Home</h1>
+            <button onClick={handleLogout}>
+                Logout
+            </button>
         </main>
     );
 }
