9b123085b95778196052fcee4b4ae0d15f197a80
ArJSarmiento <rneljan@gmail.com>
2023-09-10 13:33:19 +0800

    PWA integration

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
     watch: {
       usePolling: true,
     },
   },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,gltf}'],
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.png'],
+      manifest: {
+        name: 'PromisePH',
+        short_name: 'PromisePH',
+        description: 'Pangako o Mapapako',
+        theme_color: '#262D34',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
